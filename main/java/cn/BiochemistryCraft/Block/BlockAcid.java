680387dcd64bfbda38b4e13a394e1a6db1a1ab68
cvronmin <lung1a16@hotmail.com>
2015-07-22 14:02:38 +0800

    又一大波的更新

--- main/java/cn/BiochemistryCraft/Block/BlockAcid.java
+++ main/java/cn/BiochemistryCraft/Block/BlockAcid.java
@@ -59,8 +59,8 @@ public class BlockAcid extends Block{
 	
 	public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
     {
-        float f = 0.1F;
-        return AxisAlignedBB.getBoundingBox((double)p_149668_2_, (double)p_149668_3_, (double)p_149668_4_, (double)(p_149668_2_ + 1), (double)((float)(p_149668_3_ + 1) - f), (double)(p_149668_4_ + 1));
+        float f = 0.25F;
+        return AxisAlignedBB.getBoundingBox((double)p_149668_2_, (double)p_149668_3_, (double)p_149668_4_, (double)(p_149668_2_ + 1), (double)((float)(p_149668_3_ + f)), (double)(p_149668_4_ + 1));
     }
 
     /**
