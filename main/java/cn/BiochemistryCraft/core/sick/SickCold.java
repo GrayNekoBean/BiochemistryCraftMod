a12114707ee672b9ae728cbbd343e79aac3c0482
cvronmin <lung1a16@hotmail.com>
2015-07-26 16:24:46 +0800

    修正SickCold

--- main/java/cn/BiochemistryCraft/core/sick/SickCold.java
+++ main/java/cn/BiochemistryCraft/core/sick/SickCold.java
@@ -6,14 +6,18 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.potion.Potion;
 
-public class SickCold extends SSick{
+public class SickCold extends Sick{
 
 	protected SickCold(EntityLivingBase SickEntity) {
-		super(SickEntity);
+		super(null, 1, SickEntity);
+	}
+	
+	public SickCold(String name, int power, EntityLivingBase patient){
+		super(name, power, patient);
 	}
 
 	@Override
-	public void displayEffect() {
+	public void displayEffect(EntityLivingBase entity2) {
 		// TODO 自动生成的方法存根
 		
 	}
