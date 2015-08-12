b8f671c29c99d31f806f2dd1d5dec29a5d246d10
cvronmin <lung1a16@hotmail.com>
2015-08-12 20:50:35 +0800

    收服Shift左键物品时崩溃的bug

--- main/java/cn/BiochemistryCraft/GUI/ContainerBioExtracter.java
+++ main/java/cn/BiochemistryCraft/GUI/ContainerBioExtracter.java
@@ -10,6 +10,9 @@ import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ICrafting;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+//import net.minecraft.item.crafting.FurnaceRecipes;
+//import net.minecraft.tileentity.TileEntityFurnace;
 
 public class ContainerBioExtracter extends Container{
 	private TileentityBioExtracter tile;
@@ -80,5 +83,76 @@ public class ContainerBioExtracter extends Container{
     	}
     	this.lastTableBurnTime = this.tile.tableBurnTime;
     	this.lastMaxBurnTime = this.tile.maxBurnTime;
+    }
+    public ItemStack transferStackInSlot(EntityPlayer p_82846_1_, int p_82846_2_)
+    {
+        ItemStack itemstack = null;
+        Slot slot = (Slot)this.inventorySlots.get(p_82846_2_);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (p_82846_2_ == 2)
+            {
+                if (!this.mergeItemStack(itemstack1, 3, 39, true))
+                {
+                    return null;
+                }
+
+                slot.onSlotChange(itemstack1, itemstack);
+            }
+            else if (p_82846_2_ != 1 && p_82846_2_ != 0)
+            {
+                if (CraftingBioExtracter.getBioItem(itemstack1) != null)
+                {
+                    if (!this.mergeItemStack(itemstack1, 0, 1, false))
+                    {
+                        return null;
+                    }
+                }
+                else if (CraftingBioExtracter.getBioTime(itemstack1) != 0)
+                {
+                    if (!this.mergeItemStack(itemstack1, 2, 3, false))
+                    {
+                        return null;
+                    }
+                }
+                else if (p_82846_2_ >= 3 && p_82846_2_ < 30)
+                {
+                    if (!this.mergeItemStack(itemstack1, 30, 39, false))
+                    {
+                        return null;
+                    }
+                }
+                else if (p_82846_2_ >= 30 && p_82846_2_ < 39 && !this.mergeItemStack(itemstack1, 3, 30, false))
+                {
+                    return null;
+                }
+            }
+            else if (!this.mergeItemStack(itemstack1, 3, 39, false))
+            {
+                return null;
+            }
+
+            if (itemstack1.stackSize == 0)
+            {
+                slot.putStack((ItemStack)null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.stackSize == itemstack.stackSize)
+            {
+                return null;
+            }
+
+            slot.onPickupFromSlot(p_82846_1_, itemstack1);
+        }
+
+        return itemstack;
     }
 }
