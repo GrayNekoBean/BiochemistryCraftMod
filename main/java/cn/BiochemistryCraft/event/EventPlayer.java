36cdb19c728ca354cd2778f1119374d20f831e54
cvronmin <lung1a16@hotmail.com>
2015-09-01 16:59:10 +0800

    added method to cure sickness by having herb

--- main/java/cn/BiochemistryCraft/event/EventPlayer.java
+++ main/java/cn/BiochemistryCraft/event/EventPlayer.java
@@ -6,6 +6,7 @@ import java.util.List;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 import cn.BiochemistryCraft.core.sick.SSick;
 import cn.BiochemistryCraft.core.sick.SickPlayerInfo;
 import cn.BiochemistryCraft.core.sick.SickTrigger;
@@ -38,5 +39,13 @@ public class EventPlayer {
     	if(event.entityLiving instanceof EntityPlayer){
         	SickTrigger.trigger(TriggerType.REACH_POSISION, new int[]{(int) event.entityLiving.posX,(int) event.entityLiving.posY,(int) event.entityLiving.posZ}, (EntityLivingBase)event.entityLiving);
     	}
+    }
+    
+    public void onEaten(PlayerUseItemEvent.Finish event){
+    	for (SSick sick : SickPlayerInfo.read(event.entityPlayer)) {
+			if(sick.curetiveList.contains(event.item)){
+				SickPlayerInfo.removeSick(event.entityPlayer, sick);
+			}
+		}
     }
 }
